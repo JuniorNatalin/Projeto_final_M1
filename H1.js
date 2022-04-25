d3cd88bcc2b036f47c43f81446a0c3dc53805e6b
Natalin De Oliveira Santos Junior <93950955+JuniorNatalin@users.noreply.github.com>
2022-04-25 09:12:47 -0300

    .

---
+++ H1.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+function primeirafase (){
+    var pergunta =  (prompt ("Escolha Alternativa 1 ou 2"))
+
+
+    if(pergunta== 1){
+        window.location.href = "fase2_elf.html"
+
+    }else{
+
+        window.location.href = "Game_over.html"
+    }
+
+    }
+
+
+
+
+    
+function segundafase (){
+    var pergunta =  (prompt ("Escolha Alternativa 1 ou 2"))
+
+
+    if(pergunta== 2){
+        window.location.href = "fase3_elf.html"
+
+    }else{
+
+        window.location.href = "Game_over.html"
+    }
+
+    }
+
+
+    function terceirafase (){
+        var pergunta =  (prompt ("Escolha Alternativa 1 ou 2"))
+    
+    
+        if(pergunta== 1){
+            window.location.href = "Game_end.html"
+    
+        }else{
+    
+            window.location.href = "./Game_over.html"
+        }
+    
+        }
+
+       
+    
+
---
+++ H3.js
@@ -0,0 +1,51 @@
+
+function primeirafase (){
+    var pergunta =  (prompt ("Escolha Alternativa 1 ou 2"))
+
+
+    if(pergunta== 2){
+        window.location.href = "fase2_ORC.html"
+
+    }else{
+
+        window.location.href = "Game_over.html"
+    }
+
+    }
+
+
+
+
+    
+function segundafase (){
+    var pergunta =  (prompt ("Escolha Alternativa 1 ou 2"))
+
+
+    if(pergunta== 1){
+        window.location.href = "fase3_ORC.html"
+
+    }else{
+
+        window.location.href = "Game_over.html"
+    }
+
+    }
+
+
+    function terceirafase (){
+        var pergunta =  (prompt ("Escolha Alternativa 1 ou 2"))
+    
+    
+        if(pergunta== 1){
+            window.location.href = "Game_end.html"
+    
+        }else{
+    
+            window.location.href = "./Game_over.html"
+        }
+    
+        }
+
+       
+    
+
